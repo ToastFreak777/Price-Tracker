850bed24fbca3aa5b2ed51120d68d1875107e400
ToastFreak <gmcdonald604@gmail.com>
2025-09-27 22:17:37 -0400

    feature(api): add dummy api for testing and displaying products

--- ptracker/main/routes.py
+++ ptracker/main/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
+from ptracker.api.rapid import search
+
 main = Blueprint("main", __name__)
 
 
@@ -7,3 +9,17 @@ main = Blueprint("main", __name__)
 @main.route("/home")
 def home():
     return "<h1>Price Tracker Home page</h1>"
+
+
+@main.route("/products")
+def products():
+    products = search("Razor Deathadder V2 Pro")
+
+    for product in products[:5]:
+        print(product.get("title"))
+        print(product.get("price_cents"))
+        print(product.get("availability"))
+        print(product.get("url"))
+        print("-----")
+
+    return "<h1>Products page</h1>"
