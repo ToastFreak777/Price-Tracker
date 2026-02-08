9de1a6d2532efbd03ec911be17b2174a95e8f3b3
ToastFreak777 <gmcdonald604@gmail.com>
2026-02-07 20:18:50 -0500

    feat: mobile home page

--- ptracker/main/routes.py
+++ ptracker/main/routes.py
@@ -2,8 +2,11 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
 
+product = {"name": "Sony WH-1000XM4", "price": 299.99, "price_drop": 8.0}
+products = [product for _ in range(5)]
+
 
 @main_bp.route("/")
 @main_bp.route("/home")
 def home():
-    return render_template("main/home.html", title="Home")
+    return render_template("main/home.html", title="Home", products=products)
